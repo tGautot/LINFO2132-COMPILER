b98404861a9a31a90ab85b29eeec1c3b7578a113
angerlache <anthony.gerlache@student.uclouvain.be>
2023-05-24 15:43:05 +0200

    script for compiling and running SBD code

--- src/main/java/script.sh
+++ src/main/java/script.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# this script allows you to compile and execute a piece of code of the new language
+# run this script with as :
+# * first argument the name of the source code file (it should be in the "compiler" folder) you would like to compile and execute
+# * second argument the name of the compiled code
+# here is an example : bash script.sh simple_code_copie.txt MAIN
+
 javac -cp ".:./compiler/asm.jar" -sourcepath . compiler/Compiler.java compiler/CodeGenerator/*.java compiler/Lexer/*.java compiler/parser/*.java compiler/SemanticAnalyzer/*.java compiler/Logger/*.java
 
 java -cp ".:./compiler/asm.jar" compiler.Compiler "compiler/$1" $2
